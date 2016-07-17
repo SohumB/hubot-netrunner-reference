ae38a0300f15b256c8e6f9bf54d95aeb0ca0060e
Sohum Banerjea <sohumb@gmail.com>
2016-07-17 14:32:24 +0100

    no need for corrections anymore 😍

--- es6/netrunnerdb/corrections.js
+++
@@ -1,7 +0,0 @@
-export default [{
-  test: card => ["Ad Blitz", "Angel Arena", "Bribery", "Psychographics"].includes(card.title),
-  fix: card => card.cost = "X"
-}, {
-  test: card => ["Darwin"].includes(card.title),
-  fix: card => card.strength = "X"
-}];
--- es6/netrunnerdb/nrdb.js
+++ es6/netrunnerdb/nrdb.js
@@ -1,16 +1,10 @@
-import corrections from "./corrections";
 import request from "request-promise";
 import "babel-polyfill";
 
 export default function nrdb() {
   return request("http://netrunnerdb.com/api/cards/")
     .then(json => {
-      const cards = JSON.parse(json).map(card => corrections.reduce((acc, correction) => {
-        if (correction.test(acc)) {
-          correction.fix(acc);
-        }
-        return acc;
-      }, card));
+      const cards = JSON.parse(json);
 
       const types = cards.reduce((acc, card) => {
         acc.add(card.type_code);
